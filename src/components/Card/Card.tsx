f492586ecd59a4953434880a672a6819795f91b6
VictoryPashkova <victory.pashkova@gmail.com>
2024-08-25 19:25:45 +0600

    fix: resolve issue with card not opening on click

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 
@@ -6,12 +6,12 @@ import { setCardState } from '../../redux/reducers/app/cardsSlice';
 import { setModals } from '../../redux/reducers/app/modalsSlice';
 import { selectCards, selectComments, selectUsers } from '../../redux/reducers/selectors';
 
-const UserAvatar = styled.p<{ bgColor: string }>`
+const UserAvatar = styled.p<{ bgcolor: string }>`
   width: 20px;
   height: 20px;
   border-radius: 50%;
   overflow: hidden;
-  background-color: ${props => props.bgColor};
+  background-color: ${props => props.bgcolor};
   color: #fff;
   font-weight: 500;
   text-align: center;
@@ -19,9 +19,9 @@ const UserAvatar = styled.p<{ bgColor: string }>`
   font-size: 12px;
 `;
 
-const UserName = styled.div<{ bgColor: string }>`
+const UserName = styled.div<{ bgcolor: string }>`
   font-size: 9px;
-  background-color: ${props => props.bgColor};
+  background-color: ${props => props.bgcolor};
   color: #fff;
   font-weight: 300;
   margin-top: 5px;
@@ -78,19 +78,20 @@ const Card: React.FC<CardProps> = ({ id }) => {
     (comment) => comment.cardId === currentCard.cardId,
   ).length;
 
-  const handleOpenCardModal = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const targetElement = e.target as HTMLElement;
-    const closestCard = targetElement.closest('.card');
-    if (closestCard) {
-      const activeCardId = Number(closestCard.id);
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const handleOpenCardModal = () => {
+    if (cardRef.current) {
+      const activeCardId = Number(cardRef.current.id);
+
       dispatch(setCardState({ activeCardId, cardState: 'active' }));
       dispatch(setModals({ mainModal: true, cardModal: true }));
     }
   };
 
   return (
-    <CardStyles id={id.toString()}>
-      <UserAvatar bgColor={`#${userColor}`}>
+    <CardStyles id={id.toString()} ref={cardRef}>
+      <UserAvatar bgcolor={`#${userColor}`}>
         {currentCard.author.slice(0, 1).toLocaleUpperCase()}
       </UserAvatar>
       <button onClick={handleOpenCardModal} type="button">
@@ -99,7 +100,7 @@ const Card: React.FC<CardProps> = ({ id }) => {
           <CardComments>
             <CardCommentsValue>{commentCommentsNumber}</CardCommentsValue>
           </CardComments>
-          <UserName bgColor={`#${userColor}`}>{currentCard.author}</UserName>
+          <UserName bgcolor={`#${userColor}`}>{currentCard.author}</UserName>
         </div>
       </button>
     </CardStyles>
