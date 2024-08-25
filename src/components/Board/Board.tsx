fa8ff2e06ed7cf4ab91c8c66defd55771bcb0701
VictoryPashkova <victory.pashkova@gmail.com>
2024-08-25 18:19:58 +0600

    refactor(board): migrate styles to Styled Components

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
 import { selectColumns } from '../../redux/reducers/selectors';
 import Column from '../Column/Column';
+
+const BoardContainer = styled.div`
+  margin-left: 0;
+  margin-right: 0;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  gap: 15px;
+`;
 
 const Board: React.FC = () => {
   const columns = useSelector(selectColumns);
 
   return (      
     <DragDropContext onDragEnd={() => {}}>
-      <div className="board">
+      <BoardContainer>
         {columns.map(({ columnId, columnTitle }) => {
           if (columnId && columnTitle) {
             return <Column key={columnId} columnTitle={columnTitle} columnId={columnId} />;
           }
         })}
-      </div>
+      </BoardContainer>
     </DragDropContext>
   );
 };
